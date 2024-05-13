9d8739115dd4c4edcc4ec333f2cc151234d7b9c3
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-05-13 23:23:07 +0530

    enabled theme

--- app/home.tsx
+++ app/home.tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text ,Switch,StyleSheet} from 'react-native'
+import React, { useState } from 'react'
+import { ThemeContext } from '@/src/context/ThemeContext'
+import { Colors } from '@/constants/Colors'
 
 const home = () => {
+    const [theme, setTheme] = useState("light")
   return (
-    <View>
-      <Text>home</Text>
+    <ThemeContext.Provider value={theme}>
+    <View style={ theme === 'light' ? styles.container :  [styles.container,{backgroundColor:"#000"}]}>
+      <Switch value={theme === 'light'} onValueChange={()=> setTheme(theme === 'light' ? 'dark' : 'light')}/>
     </View>
+    </ThemeContext.Provider>
   )
 }
 
 export default home
+
+const styles = StyleSheet.create({
+    container :{
+        flex :1,
+        backgroundColor:"#fff",
+        alignItems:"center",
+        justifyContent : "center"
+    }
+})
--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,2 +1,2 @@
 import { createContext } from "react";
-export const ThemeContext = createContext("Light");
+export const ThemeContext = createContext("light");
